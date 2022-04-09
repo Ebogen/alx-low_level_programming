f40a8912253df8800afccff7807831e3803b8d01
Ebogen <ebogensamuel@gmail.com>
2022-04-09 11:39:37 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,12 @@
  * main - entry point
  *
  * betty style doc for function main goes here
+ * Return: 0
  */
 int main(void)
 {
 	int n;
+	/* This is a blank line */
 	for (n = 0; n < 26; ++n)
 		putchar('a' + n);
 
