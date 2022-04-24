e81f1e4311bd75946daf05d1166832ed0249e348
Ebogen <ebogensamuel@gmail.com>
2022-04-24 04:48:30 -0700

    fixing my new token

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ *
+ * @dest: a string
+ * @src: a string
+ * Return: a pointer to the string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char s1[98] = "src";
+	char s2[] = "dest\n";
+	char *ptr;
+
+	ptr = _strcat(s1, s2);
+
+	return (ptr);
+}
