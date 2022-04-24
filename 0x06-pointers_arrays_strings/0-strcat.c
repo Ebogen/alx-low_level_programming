687ec8e43adabe1842ca63c831ace8f602df628c
Ebogen <ebogensamuel@gmail.com>
2022-04-24 08:54:40 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	size_t dest_len = strlen(dest);
-	size_t i;
+	int count = 0, count2 = 0;
 
-	for (i = 0; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = '\0';
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+
+		count++;
+		count2++;
+	}
 	return (dest);
 }
