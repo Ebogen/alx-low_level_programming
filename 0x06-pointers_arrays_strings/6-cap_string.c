3cbfb131375b02e76e8ec34c77ed123c26526577
Ebogen <ebogensamuel@gmail.com>
2022-04-24 11:40:22 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,10 @@
 
 char *cap_string(char *s)
 {
-	int count = 0;
-	int count = i;
+	/* blank space */
+	int count 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
+	/* blank space */
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
 	count++;
