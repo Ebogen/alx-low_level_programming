37597f07da0a5830e433896bf661197c3d416fcc
Ebogen <ebogensamuel@gmail.com>
2022-05-02 20:12:11 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: parameter count
+ * @argv: parameter pointer
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
