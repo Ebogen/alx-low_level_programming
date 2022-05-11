7af9dce05b0dde6d5ec212329b59c82e3bf28459
Ebogen <ebogensamuel@gmail.com>
2022-05-10 18:53:34 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 
@@ -78,6 +81,7 @@ int get_digit(char c)
 		printf("Error\n");
 		exit(98);
 	}
+
 	return (digit);
 }
 
@@ -226,5 +230,6 @@ int main(int argc, char *argv[])
 	putchar('\n');
 	free(next_prod);
 	free(final_prod);
+
 	return (0);
 }
