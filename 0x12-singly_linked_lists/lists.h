7e636eef5c93ef35e7693ab558c4eb85f1874551
Ebogen <ebogensamuel@gmail.com>
2022-05-24 19:57:23 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,21 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stddef.h>
+
+typedef struct list_t{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
+
+/* Function prototypes */
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* _LISTS_H_ */
