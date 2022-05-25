67d99a46f0079f3a08d3820fa11c55ca3c736577
Ebogen <ebogensamuel@gmail.com>
2022-05-24 21:37:20 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list
+ * @head: pointer to list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
