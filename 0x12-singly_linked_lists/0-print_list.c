5760c6569811976a469958a5c54ed0cb35a72310
Ebogen <ebogensamuel@gmail.com>
2022-05-24 20:18:41 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: linked list
+ * Return: number of nodes in linked list
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i;
+	const list_t *tmp;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+
+	return (i);
+}
