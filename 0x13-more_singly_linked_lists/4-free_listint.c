cacaaf14142bca3427ddfa70e1b06cf61ff44ad8
Ebogen <ebogensamuel@gmail.com>
2022-05-26 10:48:16 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - a function that frees a listint_t list
+ * @head: a pointer to beginning of list
+ *Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
