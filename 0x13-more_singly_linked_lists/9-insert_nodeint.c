473821e49ed672c04c6c4616a20e9ff35a4accc5
Ebogen <ebogensamuel@gmail.com>
2022-05-26 12:28:20 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	/* we now allocate space in the memory for the new node */
-	new_node = node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
