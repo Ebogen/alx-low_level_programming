10659214627210020acfff6ea6b1521e8e40021b
Ebogen <ebogensamuel@gmail.com>
2022-07-12 20:29:12 -0700

    a dynamic library that contains C functions

---
+++ 0x18-dynamic_libraries/mainn.h
@@ -0,0 +1,10 @@
+#ifndef _MAINN_H_
+#define _MAINN_H_
+
+int addition(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+int module(int a, int b);
+
+#endif
